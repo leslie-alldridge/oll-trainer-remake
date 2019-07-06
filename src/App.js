6e746477c2c001db38553dd271e0a650b7f926a0
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-07-06 20:37:52 +1200

    init
    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SideBar from './common/sideBar'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
